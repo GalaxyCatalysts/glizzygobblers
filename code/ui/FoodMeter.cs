0eeb8574457e3f60acd4a9922aaedf446b1a5ada
thdude <30702113+thdude@users.noreply.github.com>
2021-07-09 00:05:40 -0600

    tried adding food meter, doesnt really work tho :(

--- code/ui/FoodMeter.cs
+++ code/ui/FoodMeter.cs
@@ -1,13 +1,24 @@
 using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
-using System;
-using System.Threading.Tasks;
 
 namespace MinimalExample {
     public class FoodMeter : Panel {
-        public FoodMeter() {
+
+        public Label Label;
+        public FoodMeter() 
+        {
             StyleSheet.Load("/ui/FoodMeter.scss");
+            Add.Label( "🩸", "icon" );
+		    Label = Add.Label( "100", "value" );
         }
+        public override void Tick()
+	    {
+            var player = Local.Pawn;
+            if ( player == null ) return;
+
+            Label.Text = $"{player.Health.CeilToInt()}";
+            //Label.Text = "sex";
+	    }
     }
 }
