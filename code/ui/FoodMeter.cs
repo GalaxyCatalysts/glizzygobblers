088cae616f22167aec9a5702f74b1a130266438b
unknown <56357783+superpenguin1000@users.noreply.github.com>
2021-07-09 21:40:31 -0400

    werid stuff idk

--- code/ui/FoodMeter.cs
+++ code/ui/FoodMeter.cs
@@ -1,25 +1,27 @@
 using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
+
 //using MinimalPlayer;
 
 namespace MinimalExample {
     public class FoodMeter : Panel {
 
         public Label Label;
-        public FoodMeter() 
+        public FoodMeter()	
         {
             StyleSheet.Load("/ui/FoodMeter.scss");
             Add.Label( "🌭", "icon" );
 		    Label = Add.Label( "100", "value" );
         }
-        public override void Tick()
-	    {
-            var player = Local.Pawn;
-            if ( player == null ) return;
 
-            //Label.Text = $"{player.food.CeilToInt()}";
-            Label.Text = "sex";
-	    }
+        public override void Tick()
+        {
+	        var player = Local.Pawn;
+	        if ( player == null ) return;
+
+	        //Label.Text = $"{player.food.CeilToInt()};
+	        Label.Text = "sex";
+        }
     }
 }
