86724c87e322e458235439ae6ea9830ff1198228
Isaiah <zoomer589@gmail.com>
2021-07-10 10:39:58 -0400

    added suicide script for debugging (wip)

---
+++ code/OuchMyBones.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox;
+using System;
+using System.IO;
+namespace MinimalExample
+{
+    public class OuchMyBones
+    {
+        Log.Info("Suicide Script Loaded");
+        if ( Input.Pressed( InputButton.F8 ) )
+        {
+            //TODO Finish Script
+            Log.Info("Pretend you died");
+        }
+    }
+    
+}
