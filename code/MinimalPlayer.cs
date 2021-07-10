d1341ae44ad36afa9aff6364ddce3f041b7764e4
thdude <30702113+thdude@users.noreply.github.com>
2021-07-09 20:21:30 -0600

    wtf visual studio code >:(

--- code/MinimalPlayer.cs
+++ code/MinimalPlayer.cs
@@ -63,7 +63,7 @@ namespace MinimalExample
 				ragdoll.PhysicsGroup.Velocity = EyeRot.Forward * 5000;
 				PlaySound( "fard" );
 			}
-			else if (IndexOutOfRangeException.Pressed( InputButton.Attack1)){
+			else if (Input.Pressed( InputButton.Attack1)){
 				PlaySound( "fard" );
 			}
 			
