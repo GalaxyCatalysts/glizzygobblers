aa5d44dedfbb97e901c7ff945260d6bf3f97f54c
unknown <56357783+superpenguin1000@users.noreply.github.com>
2021-07-11 14:15:23 -0400

    kritzstuffadded

---
+++ code/items/KritzPower.cs
@@ -0,0 +1,32 @@
+﻿using Sandbox;
+
+[Library( "gg_kritzpower", Title = "Kritz Powerup" )]
+[Hammer.EntityTool( "Kritz Powerup", "Kritz", "Kritz Powerup that gives massive damage" )]
+[Hammer.EditorModel( "models/kritzglizzy.vmdl" )]
+partial class KritzPower : ItemBase
+{
+	public override Type ItemType => Type.Health;
+	public virtual string WorldModelPath => "models/kritzglizzy.vmdl";
+
+	public override string PickupSound => "medkit_pickup";
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( WorldModelPath );
+	}
+
+	public override void OnCarryStart( Entity carrier )
+	{
+		base.OnCarryStart( carrier );
+
+		if ( PickupTrigger.IsValid() )
+		{
+			PickupTrigger.EnableTouch = false;
+
+			var ply = carrier as MinimalExample.MinimalPlayer;
+			ply.Health += 25;
+		}
+	}
+}
